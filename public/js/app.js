04762922a2ab08a2cb4feb5035798788bfea14fd
ilterates <iltates@gmail.com>
2017-01-03 23:55:57 -0800

    created css folder/ prices now show as dollars.

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,8 @@
 var app = angular.module('fetch', ['ngRoute', 'appRoutes', 'SiteCtrl', 'SiteService']);
 console.log("app.js");
+
+app.filter('Dollars', function () {
+    return function (price) {
+        return (price / 100).toFixed(2);
+    };
+});
--- public/js/controllers/siteCtrl.js
+++ public/js/controllers/siteCtrl.js
@@ -8,16 +8,6 @@ angular.module("SiteCtrl", [])
           url: 'http://sneakpeeq-sites.s3.amazonaws.com/interviews/ce/feeds/store.js'
         }).then(function getSuccessCallback(response) {
           $scope.data = response.data.products;
-          //$scope.products = response.data.products;
-          //console.log($scope.products);
-          var pricesCent = [];
-          var pricesDollar = [];
-          for (var i = 0; i !== 6; i++) {
-            pricesCent.push(response.data.products[i].msrpInCents);
-            pricesDollar.push((pricesCent[i]/100).toFixed(2));
-            console.log(pricesDollar +"$");
-          }
-          //console.log(response.data.products);
         }, function getErrorCallback(response) {
           console.log("Couldn't get any data", response);
         });
